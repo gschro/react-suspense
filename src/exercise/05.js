6e80860becce1944ef1ea2761820b1120e5cd963
Garrett Schroath <gtschroath@gmail.com>
2021-05-01 14:38:37 -0500

    ex 5 ec2

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,15 +4,16 @@
 import * as React from 'react'
 import {
   fetchPokemon,
+  getImageUrlForPokemon,
   PokemonInfoFallback,
   PokemonForm,
-  PokemonDataView,
   PokemonErrorBoundary,
-  getImageUrlForPokemon,
 } from '../pokemon'
 import {createResource} from '../utils'
 
-const imgSrcResourceCache = {}
+const PokemonInfo = React.lazy(() =>
+  import('../lazy/pokemon-info-render-as-you-fetch'),
+)
 
 function preloadImage(src) {
   return new Promise(resolve => {
@@ -20,18 +21,6 @@ function preloadImage(src) {
     img.src = src
     img.onload = () => resolve(src)
   })
-}
-
-function PokemonInfo({pokemonResource}) {
-  const pokemon = pokemonResource.data.read()
-  return (
-    <div>
-      <div className="pokemon-info__img-wrapper">
-        <img src={pokemonResource.image.read()} alt={pokemon.name} />
-      </div>
-      <PokemonDataView pokemon={pokemon} />
-    </div>
-  )
 }
 
 const SUSPENSE_CONFIG = {
